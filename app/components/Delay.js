0dba44e5c954c0cd6da6ee429289c8a320c9f909
Stephane Barakat <sb@Office-Mac.local>
2022-06-03 15:37:53 -0400

    delay working

--- app/components/Delay.js
+++ app/components/Delay.js
@@ -1,8 +1,37 @@
 function Delay({ controls }) {
-  console.log("controls", controls);
+  // console.log("controls", controls);
   return (
     <div>
-      <input type="range" />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="delay-time">Delay Time:</label>
+        <input
+          type="range"
+          name="delay-time"
+          min={0}
+          max={1}
+          step={0.0001}
+          onChange={(e) => {
+            // console.log(e.target.value);
+            // console.log("controls.delayTime", controls.delayTime.value);
+            controls.delayTime.value = parseFloat(e.target.value);
+          }}
+        />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="delay-feedback">Delay Feedback:</label>
+        <input
+          type="range"
+          name="delay-feedback"
+          min={0}
+          max={1}
+          step={0.0001}
+          onChange={(e) => {
+            // console.log(e.target.value);
+            // console.log("controls.delayTime", controls.delayTime.value);
+            controls.feedback.value = parseFloat(e.target.value);
+          }}
+        />
+      </div>
     </div>
   );
 }
